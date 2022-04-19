c23b14cf5b37680dc27094b694bd58d1713e54ec
Alexander Wickman <alexander.k.wickman@gmail.com>
2022-04-19 11:05:48 +0100

    added lots of logic

--- game_creation/client_files/poker_game_screen.py
+++ game_creation/client_files/poker_game_screen.py
@@ -10,28 +10,28 @@ class Game(object):
         Form.setObjectName("Form")
         Form.resize(1098, 837)
         Form.setStyleSheet("QWidget{\n"
-"    background-color: rgb(15, 102, 72);\n"
-"    \n"
-"}\n"
-"\n"
-"QLabel{\n"
-"\n"
-"}\n"
-"\n"
-"QPushButton{\n"
-"    font-family: MS Shell Dlg 2;\n"
-"    font-size: 30px;\n"
-"                \n"
-"    color: rgb(0, 85, 0);\n"
-"                \n"
-"    background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 132, 93, 255), stop:1 rgba(97, 192, 164, 255));\n"
-"                border-radius: 3px;\n"
-"                border: 2px groove rgb(0, 0, 0)\n"
-"            }\n"
-"            QPushButton:hover{\n"
-"            background-color: #e3c086;\n"
-"            outline: none\n"
-"            }")
+                           "    background-color: rgb(15, 102, 72);\n"
+                           "    \n"
+                           "}\n"
+                           "\n"
+                           "QLabel{\n"
+                           "\n"
+                           "}\n"
+                           "\n"
+                           "QPushButton{\n"
+                           "    font-family: MS Shell Dlg 2;\n"
+                           "    font-size: 30px;\n"
+                           "                \n"
+                           "    color: rgb(0, 85, 0);\n"
+                           "                \n"
+                           "    background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 132, 93, 255), stop:1 rgba(97, 192, 164, 255));\n"
+                           "                border-radius: 3px;\n"
+                           "                border: 2px groove rgb(0, 0, 0)\n"
+                           "            }\n"
+                           "            QPushButton:hover{\n"
+                           "            background-color: #e3c086;\n"
+                           "            outline: none\n"
+                           "            }")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.verticalLayout = QtWidgets.QVBoxLayout()
@@ -42,7 +42,7 @@ class Game(object):
         self.verticalLayout.addWidget(self.EMPTY)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.card1 = ExtendedCard(Form)
+        self.card1 = ExtendedCard(Form, self.my_label_clicked)
         self.card1.setMinimumSize(QtCore.QSize(200, 100))
         self.card1.setMaximumSize(QtCore.QSize(200, 300))
         self.card1.setText("")
@@ -51,28 +51,28 @@ class Game(object):
         self.card1.setScaledContents(True)
         self.card1.setObjectName("label_4")
         self.horizontalLayout.addWidget(self.card1, 0, QtCore.Qt.AlignVCenter)
-        self.card2 = ExtendedCard(Form)
+        self.card2 = ExtendedCard(Form, None)
         self.card2.setMaximumSize(QtCore.QSize(200, 300))
         self.card2.setText("")
         self.card2.setPixmap(QtGui.QPixmap(self.back_card))
         self.card2.setScaledContents(True)
         self.card2.setObjectName("label_3")
         self.horizontalLayout.addWidget(self.card2, 0, QtCore.Qt.AlignVCenter)
-        self.card3 = ExtendedCard(Form)
+        self.card3 = ExtendedCard(Form, None)
         self.card3.setMaximumSize(QtCore.QSize(200, 300))
         self.card3.setText("")
         self.card3.setPixmap(QtGui.QPixmap(self.back_card))
         self.card3.setScaledContents(True)
         self.card3.setObjectName("label_6")
         self.horizontalLayout.addWidget(self.card3, 0, QtCore.Qt.AlignVCenter)
-        self.card4 = ExtendedCard(Form)
+        self.card4 = ExtendedCard(Form, None)
         self.card4.setMaximumSize(QtCore.QSize(200, 300))
         self.card4.setText("")
         self.card4.setPixmap(QtGui.QPixmap(self.back_card))
         self.card4.setScaledContents(True)
         self.card4.setObjectName("label_5")
         self.horizontalLayout.addWidget(self.card4, 0, QtCore.Qt.AlignVCenter)
-        self.card5 = ExtendedCard(Form)
+        self.card5 = ExtendedCard(Form, None)
         self.card5.setMaximumSize(QtCore.QSize(200, 300))
         self.card5.setText("")
         self.card5.setPixmap(QtGui.QPixmap(self.back_card))
@@ -84,18 +84,31 @@ class Game(object):
 
         ClientInfo.game_gui = self
 
+        self.card1.setEnabled(True)
+        self.card1.linkActivated.connect(self.card_clicked)
+
         self.card_list = [self.card1, self.card2, self.card3, self.card4, self.card5]
         self.state = form.GameState.BETTING
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
         Form.setWindowModality(QtCore.Qt.ApplicationModal)
+
         self.open_bet_screen()
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.EMPTY.setText(_translate("Form", "EMPTY SPACE"))
+
+    def my_label_clicked(self, event, obj):
+        button = event.button()
+        modifiers = event.modifiers()
+
+        if modifiers == QtCore.Qt.NoModifier and button == QtCore.Qt.LeftButton:
+            print('worked')
+            print(event)
+            print(obj)
 
     def open_bet_screen(self):
         self.lwindow = QtWidgets.QDialog()
@@ -109,17 +122,25 @@ class Game(object):
             c = form.ExtractCard(card)
             self.card_list[i].set_values(c.suit, c.value)
 
+    def card_clicked(self, obj):
+        print('ran')
+        print(obj)
+
 
 class ExtendedCard(QtWidgets.QLabel):
-    def __init__(self, form):
+    def __init__(self, form, func):
         super(ExtendedCard, self).__init__(form)
         self.suit = ''
         self.value = 0
+        self.when_clicked = func
 
     def set_values(self, suit, value):
         self.suit = suit
         self.value = value
         self.setPixmap(QtGui.QPixmap(f'images/PNG-cards-1.3/{value}_of_{suit.lower()}.png'))
+
+    def mouseReleaseEvent(self, ev):
+        self.when_clicked(ev, self)
 
 
 if __name__ == '__main__':
